496530258cec2447b69c936490bb1a000d07bbdf
CarlosSantosSilvaTecnico <39272469+CarlosSantosSilvaTecnico@users.noreply.github.com>
2021-04-26 18:18:40 +0100

    Create dash.py

---
+++ dash.py
@@ -0,0 +1,40 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+external_stylesheets = ['mystyle.css']
+
+
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='IST Energy Monitor - Dashboard 2'),
+
+html.Div(children='''
+        Visualization of total electricity consumption at IST over the last years
+    '''),
+
+    dcc.Graph(
+        id='yearly-data',
+        figure={
+            'data': [
+                {'x': [2017, 2018, 2019], 'y': [9709, 10000, 0], 'type': 'bar', 'name': 'Total'},
+                {'x': [2017, 2018, 2019], 'y': [1440, 1605, 0], 'type': 'bar', 'name': 'Civil'},
+                {'x': [2017, 2018, 2019], 'y': [1658, 1598, 0], 'type': 'bar', 'name': 'Central'},
+                {'x': [2017, 2018, 2019], 'y': [898, 1002, 0], 'type': 'bar', 'name': 'North Tower'},
+                {'x': [2017, 2018, 2019], 'y': [1555, 1523, 0], 'type': 'bar', 'name': 'South Tower'},
+            ],
+            'layout': {
+                'title': 'IST yearly electricity consumption (kWh)'
+            }
+        }
+    ),
+
+
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
