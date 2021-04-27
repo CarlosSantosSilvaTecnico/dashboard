a0c01e915ba91455b2a16e1efaa871e809412f03
CarlosSantosSilvaTecnico <39272469+CarlosSantosSilvaTecnico@users.noreply.github.com>
2021-04-27 12:36:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,48 @@
+#https://austinlasseter.medium.com/how-to-deploy-a-simple-plotly-dash-app-to-heroku-622a2216eb73
+#https://towardsdatascience.com/deploying-your-dash-app-to-heroku-the-magical-guide-39bd6a0c586c
+#https://www.youtube.com/watch?v=b-M2KQ6_bM4
+#https://blog.heroku.com/from-project-to-productionized-python
+#https://community.plotly.com/t/deploying-your-dash-app-to-heroku-the-magical-guide/46723
+
+
 import dash
-import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Input, Output
-import pandas as pd
+import dash_html_components as html
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-df = pd.read_csv('data_class15.csv')
 
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
 
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
-app.layout = html.Div([
-    dcc.Tabs(id='tabs', value='tab-1', children=[
-        dcc.Tab(label='Table', value='tab-1'),
-        dcc.Tab(label='Graph', value='tab-2'),
-    ]),
-    html.Div(id='tabs-content')
+app.layout = html.Div(children=[
+    html.H1(children='IST Energy Monitor - Dashboard 2'),
+
+html.Div(children='''
+        Visualization of total electricity consumption at IST over the last years
+    '''),
+
+    dcc.Graph(
+        id='yearly-data',
+        figure={
+            'data': [
+                {'x': [2017, 2018, 2019], 'y': [9709, 10000, 0], 'type': 'bar', 'name': 'Total'},
+                {'x': [2017, 2018, 2019], 'y': [1440, 1605, 0], 'type': 'bar', 'name': 'Civil'},
+                {'x': [2017, 2018, 2019], 'y': [1658, 1598, 0], 'type': 'bar', 'name': 'Central'},
+                {'x': [2017, 2018, 2019], 'y': [898, 1002, 0], 'type': 'bar', 'name': 'North Tower'},
+                {'x': [2017, 2018, 2019], 'y': [1555, 1523, 0], 'type': 'bar', 'name': 'South Tower'},
+            ],
+            'layout': {
+                'title': 'IST yearly electricity consumption (kWh)'
+            }
+        }
+    ),
+
+
+
 ])
-
-@app.callback(Output('tabs-content', 'children'),
-              Input('tabs', 'value'))
-
-def render_content(tab):
-    if tab == 'tab-1':
-        return html.Div([
-            html.H3('IST Energy yearly Consumption (kWh)'),
-            generate_table(df)
-        ])
-    elif tab == 'tab-2':
-        return html.Div([
-            html.H3('IST Energy Yearly Consumption (kWh)'),
-            dcc.Graph(
-                id='yearly-data',
-                figure={
-                    'data': [
-                        {'x': df.year, 'y': df.Total, 'type': 'bar', 'name': 'Total'},
-                        {'x': df.year, 'y': df.Civil, 'type': 'bar', 'name': 'Civil'},
-                        {'x': df.year, 'y': df.Central, 'type': 'bar', 'name': 'Central'},
-                        {'x': df.year, 'y': df.NorthTower, 'type': 'bar', 'name': 'North Tower'},
-                        {'x': df.year, 'y': df.SouthTower, 'type': 'bar', 'name': 'South Tower'},
-                    ],
-                    'layout': {
-                        'title': 'IST yearly electricity consumption (MWh)'
-                    }
-                }
-            ),
-        ])
-
 
 if __name__ == '__main__':
     app.run_server(debug=False)
